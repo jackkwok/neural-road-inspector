261fe371e04e980d75500d73512bc980144e45af
Jack Kwok <ckwok@alum.mit.edu>
2017-09-23 07:08:26 -0700

    config update

--- paper_train.sh
+++ paper_train.sh
@@ -1,2 +1,2 @@
-python RoadSegmentor.py cfg/unet_dilated_blur_set2.cfg > stout/unet_dilated_blur_set2.txt;
+python RoadSegmentor.py cfg/unet_dilated_blur_set2_cont.cfg > stout/unet_dilated_blur_set2_cont.txt;
 shutdown -h 1;
